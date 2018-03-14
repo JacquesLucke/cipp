bdd7cbca9979de1954f2cd6766b50de802623073
Jacques Lucke <mail@jlucke.com>
2018-03-14 19:39:52 +0100

    parsing test

--- play2.py
+++ play2.py
@@ -1,45 +1,52 @@
-from array import array
-from exec_utils import createFunctionFromHex
-
-from x64assembler.block import Block
-from x64assembler.registers import allRegisters
-from x64assembler.instructions import (
-    AddImmToRegInstr, MovRegToRegInstr, RetInstr, 
-    PushRegInstr, PopRegInstr, SyscallInstr, 
-    MovImmToRegInstr, PushImmInstr, AddRegToRegInstr,
-    MovMemToRegInstr, MovRegToMemInstr
+from LL1parser.grammar import Grammar, NonTerminalSymbol
+from LL1parser.parser import checkIfStringMatchesGrammar
+from lexer.lexer import Lexer
+from lexer.tokens import (
+    createTokenTypeFromLetter,
+    EqualToken, IdentifierToken, IntegerToken, 
+    PlusToken, MinusToken, StarToken, SlashToken,
+    RoundBracketOpenToken, RoundBracketCloseToken,
+    WhitespaceToken, IntegerToken
 )
 
-globals().update(allRegisters)
+lexer = Lexer(
+    [PlusToken, MinusToken, StarToken, SlashToken, WhitespaceToken,
+     IdentifierToken, RoundBracketOpenToken, RoundBracketCloseToken,
+     IntegerToken],
+    ignoredTokenTypes = [WhitespaceToken]
+)
 
-N = 10000
-def genInstructions():
-    for i in range(N):
-        yield MovMemToRegInstr(rax, rdi)
-        yield AddImmToRegInstr(rax, 1)
-        yield MovRegToMemInstr(rdi, rax)
-        yield AddImmToRegInstr(rdi, 8)
-    yield RetInstr()
+expr = NonTerminalSymbol("Expr")
+expr2 = NonTerminalSymbol("Expr'")
+term = NonTerminalSymbol("Term")
+term2 = NonTerminalSymbol("Term'")
+factor = NonTerminalSymbol("Factor")
 
-instructions = list(genInstructions())
-block = Block(instructions)
-#print(block.toIntelSyntax())
+rules = {
+    expr : [
+        [term, expr2]
+    ],
+    expr2 : [
+        [PlusToken, expr],
+        [MinusToken, expr],
+        []
+    ],
+    term : [
+        [factor, term2]
+    ],
+    term2 : [
+        [StarToken, term],
+        [SlashToken, term],
+        []
+    ],
+    factor : [
+        [IntegerToken],
+        [IdentifierToken],
+        [MinusToken, factor],
+        [RoundBracketOpenToken, expr, RoundBracketCloseToken]
+    ],
+}
 
-from ctypes import c_void_p, CFUNCTYPE
-functype = CFUNCTYPE(None, c_void_p)
-func1 = createFunctionFromHex(functype, Block(instructions).toMachineCode().toHex())
+grammar = Grammar(expr, rules)
 
-def func2(numbers):
-    for i in range(len(numbers)):
-        numbers[i] += 1
-
-from timeit import timeit
-
-import numpy as np
-arr = np.zeros(N, np.int64)
-
-repetitions = 1000
-res1 = timeit("func1(arr.ctypes.data)", globals = globals(), number = repetitions)
-res2 = timeit("func2(arr)", globals = globals(), number = repetitions)
-
-print(res2 / res1)
+checkIfStringMatchesGrammar("(a + b) * c", lexer, grammar)
