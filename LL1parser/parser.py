11c5e48600c30e930f5ebe4741acf7fb40312678
Jacques Lucke <mail@jlucke.com>
2018-03-15 11:06:32 +0100

    AST construction for expression (grammar still has issues)

--- LL1parser/parser.py
+++ LL1parser/parser.py
@@ -3,11 +3,12 @@ from . token_stream import TokenStream, EOFToken
 
 def checkIfStringMatchesGrammar(string, lexer, grammar):
     tokens = list(lexer.tokenize(string))
-    checkIfTokenStreamMatchesGrammar(TokenStream(tokens), grammar)
+    return checkIfTokenStreamMatchesGrammar(TokenStream(tokens), grammar)
 
 def checkIfTokenStreamMatchesGrammar(tokens, grammar):
     table = grammar.createParsingTable()
     stack = [EOFToken, grammar.start]
+    treeStack = []
 
     while len(stack) > 0:
         stackTop = stack.pop()
@@ -17,13 +18,51 @@ def checkIfTokenStreamMatchesGrammar(tokens, grammar):
         if isinstance(stackTop, NonTerminalSymbol):
             entry = (stackTop, tokenType)
             if entry in table:
-                stack.extend(table[entry][::-1])
+                production = table[entry]
+                node = ParseTreeNode(stackTop, production)
+                stack.append(node)
+                stack.extend(reversed(production))
             else:
                 raise Exception(f"no parser table entry for {entry}")
+        elif isinstance(stackTop, ParseTreeNode):
+            node = stackTop
+            count = node.elementCount
+            if count > 0:
+                node.setChildren(treeStack[-count:])
+                del treeStack[-count:]
+            else:
+                node.setChildren([])
+            treeStack.append(node)
         elif stackTop == tokenType:
-            tokens.take(1)
+            treeStack.append(tokens.take(1))
         else:
             raise Exception(f"unexpected token: {token}")
 
-    if len(stack) > 0:
-        raise Exception("missing tokens at the end")
+    return treeStack[0]
+
+class ParseTreeNode:
+    def __init__(self, symbol, production):
+        self.symbol = symbol
+        self.production = production
+
+    def setChildren(self, children):
+        self.children = children
+
+    @property
+    def elementCount(self):
+        return len(self.production)
+
+    def __repr__(self):
+        return f"<Node: {self.symbol}>"
+
+    def toTreeString(self):
+        return "\n".join(self.iterTreeStringLines())
+
+    def iterTreeStringLines(self):
+        yield f"{self.symbol}:"
+        for child in self.children:
+            if isinstance(child, ParseTreeNode):
+                for line in child.iterTreeStringLines():
+                    yield "   |" + line
+            else:
+                yield "   |" + str(child)
--- play2.py
+++ play2.py
@@ -16,37 +16,131 @@ lexer = Lexer(
     ignoredTokenTypes = [WhitespaceToken]
 )
 
-expr = NonTerminalSymbol("Expr")
-expr2 = NonTerminalSymbol("Expr'")
-term = NonTerminalSymbol("Term")
-term2 = NonTerminalSymbol("Term'")
-factor = NonTerminalSymbol("Factor")
+Expr = NonTerminalSymbol("Expr")
+Expr2 = NonTerminalSymbol("Expr'")
+Term = NonTerminalSymbol("Term")
+Term2 = NonTerminalSymbol("Term'")
+Factor = NonTerminalSymbol("Factor")
 
 rules = {
-    expr : [
-        [term, expr2]
+    Expr : [
+        [Term, Expr2]
     ],
-    expr2 : [
-        [PlusToken, expr],
-        [MinusToken, expr],
+    Expr2 : [
+        [PlusToken, Expr],
+        [MinusToken, Expr],
         []
     ],
-    term : [
-        [factor, term2]
+    Term : [
+        [Factor, Term2]
     ],
-    term2 : [
-        [StarToken, term],
-        [SlashToken, term],
+    Term2 : [
+        [StarToken, Term],
+        [SlashToken, Term],
         []
     ],
-    factor : [
+    Factor : [
         [IntegerToken],
         [IdentifierToken],
-        [MinusToken, factor],
-        [RoundBracketOpenToken, expr, RoundBracketCloseToken]
+        [MinusToken, Factor],
+        [RoundBracketOpenToken, Expr, RoundBracketCloseToken]
     ],
 }
 
-grammar = Grammar(expr, rules)
+grammar = Grammar(Expr, rules)
 
-checkIfStringMatchesGrammar("(a + b) * c", lexer, grammar)
+tree = checkIfStringMatchesGrammar("4+5*(1 -4)+a", lexer, grammar)
+print(tree.toTreeString())
+
+class ExprAST:
+    def __new__(cls, root):
+        term, expr2 = root.children
+        if expr2.production == rules[Expr2][0]:
+            return PlusNode(term, expr2.children[1])
+        elif expr2.production == rules[Expr2][1]:
+            return MinusNode(term, expr2.children[1])
+        elif expr2.production == rules[Expr2][2]:
+            return TermAST(term)
+
+class TermAST:
+    def __new__(cls, root):
+        factor, term2 = root.children
+        if term2.production == rules[Term2][0]:
+            return MulNode(factor, term2.children[1])
+        elif term2.production == rules[Term2][1]:
+            return DivNode(factor, term2.children[1])
+        elif term2.production == rules[Term2][2]:
+            return FactorAST(factor)
+
+class FactorAST:
+    def __new__(cls, root):
+        if root.production == rules[Factor][0]:
+            return IntegerNode(root.children[0][0])
+        elif root.production == rules[Factor][1]:
+            return IdentifierNode(root.children[0][0])
+        elif root.production == rules[Factor][2]:
+            return NegateNode(root.children[1])
+        elif root.production == rules[Factor][3]:
+            return ExprAST(root.children[1])
+
+class ASTNode:
+    def toTreeString(self):
+        return "\n".join(self.iterTreeStringLines())
+
+    def iterTreeStringLines(self):
+        yield self.getName()
+        for child in self.children:
+            for line in child.iterTreeStringLines():
+                yield f"  |{line}" 
+
+    def getName(self):
+        return type(self).__name__
+
+
+class PlusNode(ASTNode):
+    def __init__(self, term, expr):
+        self.a = TermAST(term)
+        self.b = ExprAST(expr)
+        self.children = [self.a, self.b]
+
+class MinusNode(ASTNode):
+    def __init__(self, term, expr):
+        self.a = TermAST(term)
+        self.b = ExprAST(expr)
+        self.children = [self.a, self.b]
+
+class MulNode(ASTNode):
+    def __init__(self, factor, term):
+        self.a = FactorAST(factor)
+        self.b = TermAST(term)
+        self.children = [self.a, self.b]
+
+class DivNode(ASTNode):
+    def __init__(self, factor, term):
+        self.a = FactorAST(factor)
+        self.b = TermAST(term)
+        self.children = [self.a, self.b]
+
+class IntegerNode(ASTNode):
+    def __init__(self, token):
+        self.value = token.number
+        self.children = []
+
+    def getName(self):
+        return str(self.value)
+
+class IdentifierNode(ASTNode):
+    def __init__(self, token):
+        self.value = token.content
+        self.children = []
+
+    def getName(self):
+        return self.value
+
+class NegateNode(ASTNode):
+    def __init__(self, factor):
+        self.a = FactorAST(factor)
+        self.children = [self.a]
+
+parsedExpression = ExprAST(tree)
+print(parsedExpression.toTreeString())
