13e9f50c64b82aefed9273b4eb6400159368fd7c
Jacques Lucke <mail@jlucke.com>
2018-03-17 18:52:57 +0100

    block also outputs labels in Intel Syntax now

--- x64assembler/block.py
+++ x64assembler/block.py
@@ -3,10 +3,17 @@ from . bits import Bits
 class Block:
     def __init__(self, instructions = [], labels = {}):
         self.instructions = instructions
-        self.labels = labels
+        self.instructionByLabel = labels
+        self.labelByInstruction = {instr : label for label, instr in labels.items()}
 
     def toIntelSyntax(self):
-        return "\n".join(instr.toIntelSyntax() for instr in self.instructions)
+        return "\n".join(self._iterIntelSyntaxLines())
+
+    def _iterIntelSyntaxLines(self):
+        for instr in self.instructions:
+            if instr in self.labelByInstruction:
+                yield f"{self.labelByInstruction[instr]}:"
+            yield str(instr)
 
     def toMachineCode(self):
         position = 0
@@ -25,10 +32,10 @@ class Block:
             positionOfNextInstr = positionByInstruction[instruction] + machineCode.byteLength
 
             for link in instruction.getLinks():
-                if link.label not in self.labels:
+                if link.label not in self.instructionByLabel:
                     raise Exception(f"cannot find label: {link.label}")
 
-                linkedInstruction = self.labels[link.label]
+                linkedInstruction = self.instructionByLabel[link.label]
                 linkedPosition = positionByInstruction[linkedInstruction]
                 offset = linkedPosition - positionOfNextInstr
                 machineCode = link.insertOffset(machineCode, offset)
