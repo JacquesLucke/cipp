9375c44ffa743aae687b27fe91fc358f0997f95a
Jacques Lucke <mail@jlucke.com>
2018-03-17 10:32:48 +0100

    cleanup

--- cipp_parser/token_stream.py
+++ cipp_parser/token_stream.py
@@ -1,25 +1,25 @@
-from lexer.token import Token
+from lexer import Token
 
 class TokenStream:
     def __init__(self, tokens):
         self.tokens = tokens
         self.position = 0
 
+    def takeNext(self):
+        return self.take(1)[0]
+
     def take(self, n):
         tokens = self.getLookahead(n)
         self.position += n
         return tokens
 
-    def takeNext(self):
-        return self.take(1)[0]
+    def peekNext(self):
+        return self.getLookahead(1)[0]
 
     def getLookahead(self, n):
         if len(self) < n:
             raise Exception(f"cannot look ahead {n} tokens, only {len(self)} available")
         return self.tokens[self.position:self.position+n]
-
-    def peekNext(self):
-        return self.getLookahead(1)[0]
 
     def __len__(self):
         return len(self.tokens) - self.position
