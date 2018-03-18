d97d754d101996dcd269c15110f2fc083b85a746
Jacques Lucke <mail@jlucke.com>
2018-03-18 15:26:44 +0100

    update tests

--- cipp/test_parser.py
+++ cipp/test_parser.py
@@ -1,31 +1,32 @@
 import unittest
+from . import ast
 from . import parser
 tokenize = parser.stringToTokenStream
 
 class TestSelectStatementParser(unittest.TestCase):
     def testBlock(self):
         stmt = self.parseStatement("{a=b;c=d;}")
-        self.assertIsInstance(stmt, parser.BlockStmtAST)
+        self.assertIsInstance(stmt, ast.BlockStmt)
 
     def testReturn(self):
         stmt = self.parseStatement("return 3;")
-        self.assertIsInstance(stmt, parser.ReturnStmtAST)
+        self.assertIsInstance(stmt, ast.ReturnStmt)
 
     def testLet(self):
         stmt = self.parseStatement("let int variable = 42;")
-        self.assertIsInstance(stmt, parser.LetStmtAST)
+        self.assertIsInstance(stmt, ast.LetStmt)
 
     def testWhile(self):
         stmt = self.parseStatement("while (1 < 2) {}")
-        self.assertIsInstance(stmt, parser.WhileStmtAST)
+        self.assertIsInstance(stmt, ast.WhileStmt)
 
     def testIf(self):
         stmt = self.parseStatement("if (1 < 2) {}")
-        self.assertIsInstance(stmt, parser.IfStmtAST)
+        self.assertIsInstance(stmt, ast.IfStmt)
 
     def testIfElse(self):
         stmt = self.parseStatement("if (1) {} else {}")
-        self.assertIsInstance(stmt, parser.IfElseStmtAST)
+        self.assertIsInstance(stmt, ast.IfElseStmt)
 
     def parseStatement(self, code):
         tokens = tokenize(code)
@@ -33,18 +34,18 @@ class TestSelectStatementParser(unittest.TestCase):
 
 class TestParseFunctionCall(unittest.TestCase):
     def testNormal(self):
-        ast = self.parseFunctionCall("@hello(3+4, 2*a)")
-        self.assertEqual(ast.functionName, "hello")
-        self.assertEqual(len(ast.arguments), 2)
+        node = self.parseFunctionCall("@hello(3+4, 2*a)")
+        self.assertEqual(node.functionName, "hello")
+        self.assertEqual(len(node.arguments), 2)
 
     def testMissingAt(self):
         with self.assertRaises(Exception):
             self.parseFunctionCall("hello(1, 1)")
 
     def testNoArguments(self):
-        ast = self.parseFunctionCall("@test()")
-        self.assertEqual(ast.functionName, "test")
-        self.assertEqual(len(ast.arguments), 0)  
+        node = self.parseFunctionCall("@test()")
+        self.assertEqual(node.functionName, "test")
+        self.assertEqual(len(node.arguments), 0)  
 
     def testForgottenComma(self):
         with self.assertRaises(Exception):
@@ -52,63 +53,63 @@ class TestParseFunctionCall(unittest.TestCase):
 
     def parseFunctionCall(self, code):
         tokens = tokenize(code)
-        ast = parser.parseFunctionCall(tokens)
-        self.assertIsInstance(ast, parser.FunctionCallAST)
-        return ast
+        node = parser.parseFunctionCall(tokens)
+        self.assertIsInstance(node, ast.FunctionCall)
+        return node
 
 class TestParseExpression(unittest.TestCase):
     def testSimpleAdd(self):
-        ast = self.parseExpression("3 + a")
-        self.assertIsInstance(ast, parser.AddSubExprAST)
-        self.assertEqual(ast.terms[0].expr.value, 3)
-        self.assertEqual(ast.terms[1].expr.name, "a")
+        node = self.parseExpression("3 + a")
+        self.assertIsInstance(node, ast.AddSubExpr)
+        self.assertEqual(node.terms[0].expr.value, 3)
+        self.assertEqual(node.terms[1].expr.name, "a")
 
     def testAddAndSub(self):
-        ast = self.parseExpression("1 + a - 4 - 3 + test")
-        self.assertIsInstance(ast, parser.AddSubExprAST)
-        for term, operation in zip(ast.terms, "++--+"):
+        node = self.parseExpression("1 + a - 4 - 3 + test")
+        self.assertIsInstance(node, ast.AddSubExpr)
+        for term, operation in zip(node.terms, "++--+"):
             self.assertEqual(term.operation, operation)
-        self.assertEqual(ast.terms[0].expr.value, 1)
-        self.assertEqual(ast.terms[1].expr.name, "a")
-        self.assertEqual(ast.terms[2].expr.value, 4)
-        self.assertEqual(ast.terms[3].expr.value, 3)
-        self.assertEqual(ast.terms[4].expr.name, "test")
+        self.assertEqual(node.terms[0].expr.value, 1)
+        self.assertEqual(node.terms[1].expr.name, "a")
+        self.assertEqual(node.terms[2].expr.value, 4)
+        self.assertEqual(node.terms[3].expr.value, 3)
+        self.assertEqual(node.terms[4].expr.name, "test")
 
     def testMulAndDiv(self):
-        ast = self.parseExpression("1 * a / 4 / 3 * test")
-        self.assertIsInstance(ast, parser.MulDivExprAST)
-        for term, operation in zip(ast.terms, "**//*"):
+        node = self.parseExpression("1 * a / 4 / 3 * test")
+        self.assertIsInstance(node, ast.MulDivExpr)
+        for term, operation in zip(node.terms, "**//*"):
             self.assertEqual(term.operation, operation)
-        self.assertEqual(ast.terms[0].expr.value, 1)
-        self.assertEqual(ast.terms[1].expr.name, "a")
-        self.assertEqual(ast.terms[2].expr.value, 4)
-        self.assertEqual(ast.terms[3].expr.value, 3)
-        self.assertEqual(ast.terms[4].expr.name, "test")
+        self.assertEqual(node.terms[0].expr.value, 1)
+        self.assertEqual(node.terms[1].expr.name, "a")
+        self.assertEqual(node.terms[2].expr.value, 4)
+        self.assertEqual(node.terms[3].expr.value, 3)
+        self.assertEqual(node.terms[4].expr.name, "test")
 
     def testContant(self):
-        ast = self.parseExpression("123")
-        self.assertIsInstance(ast, parser.ConstIntAST)
-        self.assertEqual(ast.value, 123)
+        node = self.parseExpression("123")
+        self.assertIsInstance(node, ast.ConstInt)
+        self.assertEqual(node.value, 123)
 
     def testVariable(self):
-        ast = self.parseExpression("qwe")
-        self.assertIsInstance(ast, parser.VariableAST)
+        node = self.parseExpression("qwe")
+        self.assertIsInstance(node, ast.Variable)
 
     def testConstantInBrackets(self):
-        ast = self.parseExpression("((9))")
-        self.assertIsInstance(ast, parser.ConstIntAST)
-        self.assertEqual(ast.value, 9)
+        node = self.parseExpression("((9))")
+        self.assertIsInstance(node, ast.ConstInt)
+        self.assertEqual(node.value, 9)
 
     def testFunctionCallInExpression(self):
-        ast = self.parseExpression("(3+5)*@test(12+a)")
-        self.assertIsInstance(ast, parser.MulDivExprAST)
-        self.assertIsInstance(ast.terms[1].expr, parser.FunctionCallAST)
-        self.assertEqual(ast.terms[1].expr.functionName, "test")
-        self.assertEqual(len(ast.terms[1].expr.arguments), 1)
-        self.assertEqual(ast.terms[1].expr.arguments[0].terms[0].expr.value, 12)
+        node = self.parseExpression("(3+5)*@test(12+a)")
+        self.assertIsInstance(node, ast.MulDivExpr)
+        self.assertIsInstance(node.terms[1].expr, ast.FunctionCall)
+        self.assertEqual(node.terms[1].expr.functionName, "test")
+        self.assertEqual(len(node.terms[1].expr.arguments), 1)
+        self.assertEqual(node.terms[1].expr.arguments[0].terms[0].expr.value, 12)
 
     def parseExpression(self, code):
         tokens = tokenize(code)
-        ast = parser.parseExpression(tokens)
-        self.assertIsInstance(ast, parser.ExpressionAST)
-        return ast
+        node = parser.parseExpression(tokens)
+        self.assertIsInstance(node, ast.Expression)
+        return node
